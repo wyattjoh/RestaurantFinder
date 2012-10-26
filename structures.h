c643342694a18ab064c2733679856d6daf797309
Wyatt Johnson <wyattjoh@gmail.com>
2012-10-26 09:37:46 -0600

    Added Restaurant structure to structures.h

--- structures.h
+++ structures.h
@@ -7,4 +7,12 @@ typedef struct {
 	uint16_t r;
 } coord_t;
 
+typedef struct {
+  int32_t latitude_scaled;  // Scaled by 100 000
+  int32_t longitude_scaled; // Scaled by 100 000
+  int8_t  rating;           // Average rating, scale from 0 - 10
+  char name[55];            // Always null terminated
+  
+} Restaurant;
+
 #endif
